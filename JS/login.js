9b71eb15eafe057ed99b85bffeb6f5689916656b
janshaaa <83596419+janshaaa@users.noreply.github.com>
2022-05-19 19:39:33 +0800

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,6 @@
 function verifyLogin() {
+  // Valid username: Glitch password: Glitch123
+  
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
@@ -8,6 +10,7 @@ function verifyLogin() {
   } else if (!(username == "Glitch" && password == "Glitch123")) {
     document.getElementById("error").innerHTML =
       "*Username or password does not exist!*";
+    
     return false;
   } else {
     window.location.href = "home.html";
